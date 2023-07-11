e1463c5f235a4c25163c3965e5af95f06963436b
Jan Svec <isvecjan@fit.vutbr.cz>
2023-07-11 17:52:53 +0200

    Update eval_ppl_with_pretrained_lm.py - add 'max_len' feature - reworked implementations

--- lms/src/eval_ppl_with_pretrained_lm.py
+++ lms/src/eval_ppl_with_pretrained_lm.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# author : Santosh Kesiraju
+# author : Santosh Kesiraju, Jan Svec
 # e-mail : kcraj2[AT]gmail[DOT]com, kesiraju[AT]fit[DOT]vutbr[DOT]cz
+#        : john.swabe[AT]gmail[DOT]com, isvecjan[AT]fit[DOT]vutbr[DOT]cz
 # Date created : 29 Jun 2023
 # Last modified : 29 Jun 2023
 
@@ -11,57 +12,17 @@
 
 import os
 import sys
-import logging
-import argparse
-import numpy as np
+import json
 import torch
-from tqdm import tqdm
-from utils import load_text, create_logger
+import pickle
+import argparse
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast
-
-
-def prepare_indep_utt_dataset(texts, tokenizer):
-    """Prepare dataset that treats utterances independently"""
-
-    logger = logging.getLogger()
-
-    bsize = 32
-    start = 0
-    end = start + bsize
-    input_ids = []
-    # input_lens = []
-    target_ids = []
-
-    ign = 0
-
-    while start < len(texts):
-
-        if end > len(texts):
-            end = len(texts)
-
-        encodings = tokenizer(texts[start:end])
-
-        for inp in encodings.input_ids:
-            if len(inp) == 1:
-                ign += 1
-                continue
-            input_ids.append(torch.LongTensor(inp))
-            target_ids.append(torch.LongTensor(inp))
-            # input_lens.append(len(inp))
-
-        start = end
-        end += bsize
-
-    if ign > 0:
-        logger.info("Ignoring single word utterances %d", ign)
-
-    return input_ids, target_ids
-
-
-def prepare_max_seq_len_dataset(texts: list, tokenizer, max_seq_len):
-    """Prepare input seqs and targets based on max seq length of the model"""
-
-    pass
+from pathlib import Path
+from time import time
+from utils import (
+    FisherTextDatasetIndep, FisherTextDatasetMaxLen,
+    create_logger,
+    compute_ppl_per_recording)
 
 
 def main():
@@ -69,69 +30,141 @@ def main():
 
     args = parse_arguments()
 
+    if args.download_only:
+        model_id = args.model
+        _ = GPT2LMHeadModel.from_pretrained(model_id)
+        _ = GPT2TokenizerFast.from_pretrained(model_id)
+        sys.exit(0)
+
+    # if args.offline:
+    #     os.environ["HF_HUB_OFFLINE"] = "1"
+    #     os.environ["HF_DATASETS_OFFLINE"] = "1"
+    #     os.environ["TRANSFORMERS_OFFLINE"] = "1"
+    #     os.environ["HF_EVALUATE_OFFLINE"] = "1"
+
     os.makedirs(args.out_dir, exist_ok=True)
 
+    path_out_dir = Path(args.out_dir)
     device = "cuda" if args.cuda else "cpu"
 
     # prepare common file prefix based on input args
-
     model_id = args.model
     base = os.path.basename(args.in_file).rsplit(".", 1)[0]
     context = args.context_type
     pfx = f"{model_id}_{context}_{base}"
 
     # Create logger
+    logger = create_logger(path_out_dir / f"{pfx}.log", args.verbose)
 
-    logger = create_logger(os.path.join(args.out_dir, f"{pfx}"), args.verbose)
-    logger.info("CUDA_VISIBLE_DEVICES=" + os.environ.get("CUDA_VISIBLE_DEVICES"))
-
-    # out file to save negative LLHs for each input - can be used later to compute PPL
-    out_nll_file = os.path.join(args.out_dir, f"{pfx}_nlls.txt")
-
-    # Load the input text line-by-line
-    texts = load_text(args.in_file)
-    logger.info("In text lines in %s: %d", args.in_file, len(texts))
+    # Get GPU (BUT style)
+    if args.cuda and args.but_gpu:
+        from safe_gpu import safe_gpu
+        safe_gpu.claim_gpus(logger=logger)
 
     # Load pre-trained model and tokenizer
     model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
     tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
 
-    # Tokenize the input text and convert to torch Tensors
-    if context == "indep":
-        logger.info(
-            "Treating each sentence independently. Sentences with 1 word will be ignored."
-        )
-        all_input_ids, all_target_ids = prepare_indep_utt_dataset(texts, tokenizer)
-
-    elif context == "max_len":
-        max_len = model.config.n_positions
-        logger.info("Max seq length of the model is %d", max_len)
-        all_input_ids, all_target_ids = prepare_max_seq_len_dataset(texts, tokenizer)
+    if args.context_type == "indep":
+        dataset = FisherTextDatasetIndep(args.in_file, tokenizer, batch_size=args.bsize)
+        utt_ids = dataset.utt_ids
+    elif args.context_type == "max_len":
+        dataset = FisherTextDatasetMaxLen(args.in_file, tokenizer, max_len=model.config.n_positions, batch_size=args.bsize)
     else:
-        print("Context type", context, "not implemented", file=sys.stderr)
-        sys.exit()
+        raise ValueError(f"Wrong set 'context_type' as {args.context_type}")
 
-    # forward and get negative llh for each input sequence
     nlls = []
-    for i in tqdm(range(len(all_input_ids))):
+    stime = time()
+    with torch.no_grad():
+        if args.context_type == "indep":
+            for batch_text_ids in dataset:
+                batch_text_ids = torch.LongTensor(batch_text_ids).to(device=device)
+                target_ids = batch_text_ids.clone()
 
-        input_ids = all_input_ids[i].to(device=device)
-        target_ids = all_target_ids[i].to(device=device)
+                outputs = model(batch_text_ids)
 
-        with torch.no_grad():
-            outputs = model(input_ids, labels=target_ids)
+                xen_loss = torch.nn.CrossEntropyLoss(reduction='none')
 
-            # loss is calculated using CrossEntropyLoss which averages over
-            # valid labels
-            # N.B. the model only calculates loss over trg_len - 1 labels,
-            # because it internally shifts the labels to the left by 1.
-            neg_llh = outputs.loss
+                # ignore the logits for the last token
+                shifted_logits = outputs.logits[..., :-1, :].contiguous()
 
-        nlls.append(neg_llh)
+                # ignore the targets for the first token
+                shifted_targets = target_ids[..., 1:].contiguous()
 
-    ppl = torch.exp(torch.stack(nlls).mean())
-    np.savetxt(out_nll_file, torch.stack(nlls).cpu().numpy())
-    logger.info("PPL: %.2f", ppl.cpu().item())
+                # compute cross entropy loss and return it for every token
+                neg_llh = xen_loss(torch.transpose(shifted_logits, 1, 2), shifted_targets)
+
+                # append to the list of nlls
+                nlls.extend(neg_llh.cpu().numpy().tolist())
+        elif args.context_type == "max_len":
+            i_recording = 0
+            rec_ids = []
+            nsamples = 0
+            act_rec = None
+            for i, (batch_text_ids, rec_id, first_batch, last_batch) in enumerate(dataset):
+                print(f"\r Batch: {i} , Actual rec: {act_rec} ({i_recording}/{dataset.nrecording}), Procesed sentences: {nsamples} / {dataset.nsentence}", end=" ", file=sys.stderr)
+                nsamples += len(batch_text_ids)
+
+                batch_text_ids = torch.LongTensor(batch_text_ids).to(device=device)
+                target_ids = batch_text_ids.clone()
+
+                outputs = model(batch_text_ids)
+
+                xen_loss = torch.nn.CrossEntropyLoss(reduction='none')
+                if first_batch:
+                    xen_loss = torch.nn.CrossEntropyLoss(reduction='none')
+                    print()
+                    act_rec = rec_id[0]
+                    i_recording += 1
+
+                    # ignore the logits for the last token
+                    shifted_logits = outputs.logits[..., :-1, :].contiguous()
+
+                    # ignore the targets for the first token
+                    shifted_targets = target_ids[..., 1:].contiguous()
+
+                    # compute cross entropy loss and return it for every token
+                    neg_llh = xen_loss(torch.transpose(shifted_logits, 1, 2), shifted_targets)
+
+                    # append to the list of nlls
+                    nlls.extend(neg_llh.cpu().numpy().tolist())
+                    rec_ids.extend(rec_id)
+                else:
+                    xen_loss = torch.nn.CrossEntropyLoss(reduction='none')
+                    # ignore the logits for the last token
+                    shifted_logits = outputs.logits[..., :-1, :].contiguous()
+
+                    # ignore the targets for the first token
+                    shifted_targets = target_ids[..., 1:].contiguous()
+
+                    # compute cross entropy loss and return it for every token
+                    neg_llh = xen_loss(torch.transpose(shifted_logits, 1, 2), shifted_targets)
+
+                    # append to the list of nlls
+                    nlls.extend([[_v] for _v in neg_llh[:,-1].cpu().numpy().tolist()])
+                    rec_ids.extend(rec_id)
+        else:
+            raise NotImplementedError
+        print()
+
+    if args.context_type == "indep":
+        ids = utt_ids
+    elif args.context_type == "max_len":
+        ids = rec_ids
+
+    assert len(nlls) == len(ids), f"nlls {len(nlls)} != utt_ids {len(ids)}"
+
+    rec_id2nlls, rec_id2ppl = compute_ppl_per_recording(nlls, ids)
+
+    with open(path_out_dir / "rec_id2nlls.pkl", "wb") as fpw:
+        pickle.dump(rec_id2nlls, fpw)
+
+    with open(path_out_dir / "rec_id2ppl.json", "w", encoding="utf-8") as fpw:
+        json.dump(rec_id2ppl, fpw, indent=2, ensure_ascii=False)
+
+    logger.info(f"Saved in {args.out_dir} Time taken {time() - stime:.2f} sec")
+
+    return 0
 
 
 def parse_arguments():
@@ -141,33 +174,44 @@ def parse_arguments():
         description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument(
-        "--in_file",
+        "--in_file", "-in_file", "-i",
         required=True,
         help="path to input text file on which PPL shall be computed",
     )
-    parser.add_argument("--ftype", choices=['text', 'key'], required=True,
-                        help="""input file type. text is plain text line by line,
-                        key is where first column in utterance with time_stamp (kaldi format)
-                        which will help in figuring out the chronology of utterances in
-                        in a recording"""
-                        )
+    # parser.add_argument(
+    #     "--ftype",
+    #     choices=['text', 'key'],
+    #     required=True,
+    #     help="""input file type. text is plain text line by line,
+    #     key is where first column in utterance with time_stamp (kaldi format)
+    #     which will help in figuring out the chronology of utterances in
+    #     in a recording""",
+    # )
     parser.add_argument(
-        "--out_dir", required=True, help="path to out dir where results are stored"
+        "--out_dir", "--out_dir", "-o",
+        required=True,
+        help="path to out dir where results are stored",
     )
     parser.add_argument(
-        "--model",
+        "--bsize", "--batch_size", "-bsize", "-batch_size", "--sb", "-sb",
+        type=int,
+        default=128,
+        help="max batch size"
+    )
+    parser.add_argument(
+        "--model", "-model", "-m",
         type=str,
         default="gpt2",
         choices=["gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"],
         help="huggingface model",
     )
     parser.add_argument(
-        "--context_type",
+        "--context_type", "-context_type", "--ct", "-ct",
         choices=["indep", "max_len"],
         default="indep",
         type=str,
         help="""How much context to use? indep is independent utterances, max_len is
-                        max length constrained by the pretrained model which could be 512 or 1024 tokens""",
+        max length constrained by the pretrained model which could be 512 or 1024 tokens""",
     )
     parser.add_argument(
         "--no_cuda",
@@ -175,19 +219,35 @@ def parse_arguments():
         help="do not use cuda device, by default uses cuda",
     )
     parser.add_argument(
-        "--verbose", action="store_true", help="print logging info on stdout"
+        "--offline",
+        action="store_true",
+        help="do not use internet(connection), by default yes",
+    )
+    parser.add_argument(
+        "--download_only",
+        action="store_true",
+        help="Download only model from HuggingFace",
+    )
+    parser.add_argument(
+        "--but_gpu", "-but_gpu",
+        action="store_true",
+        help="Use BUT's procedure for access to GPU",
+    )
+    parser.add_argument(
+        "--verbose", "-v",
+        action="store_true",
+        help="print logging info on stdout",
     )
     args = parser.parse_args()
 
     args.cuda = not args.no_cuda
 
-    if args.context_type == "max_len":
-        if args.ftype != "key":
-             print("Input ftype must be 'key' when using 'max_len' context")
-             sys.exit()
+    # if args.context_type == "max_len" and args.ftype != "key":
+    #     print("Input ftype must be 'key' when using 'max_len' context")
+    #     sys.exit()
 
     return args
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
