8f9749d982311d8eb8fb1f69eeabe01c872f9c0e
Jan Svec <isvecjan@fit.vutbr.cz>
2023-07-19 17:49:03 +0200

    Re-design indep/max_len processing, multiple checkpoints

--- lms/src/eval_ppl_with_pretrained_lm.py
+++ lms/src/eval_ppl_with_pretrained_lm.py
@@ -13,7 +13,6 @@
 import os
 import sys
 import json
-# from typing import Any
 import torch
 import pickle
 import argparse
@@ -21,11 +20,15 @@ from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 from pathlib import Path
 from time import time
 from utils import (
-    Processor, FisherTextDataset, FisherTextDatasetIndep,
+    FisherTextDatasetIndep,
+    FisherTextDatasetMaxLen,
     create_logger,
-    compute_ppl_per_recording
+    compute_ppl_per_recording,
+    save_checkpoint,
+    load_checkpoint,
 )
 from tqdm import tqdm
+
 
 def main():
     """Main method"""
@@ -55,6 +58,7 @@ def main():
     # Get GPU (BUT style)
     if args.cuda and args.but_gpu:
         from safe_gpu import safe_gpu
+
         safe_gpu.claim_gpus(logger=logger)
 
     # Load pre-trained model and tokenizer
@@ -63,51 +67,77 @@ def main():
 
     if args.context_type == "indep":
         dataset = FisherTextDatasetIndep(args.in_file, tokenizer, batch_size=args.bsize)
-        utt_ids = dataset.utt_ids
     elif args.context_type == "max_len":
-        # dataset = FisherTextDatasetMaxLen(args.in_file, tokenizer, max_len=model.config.n_positions, batch_size=args.bsize)
-        dataset = FisherTextDataset(args.in_file, tokenizer,
-                                    max_len=model.config.n_positions,
-                                    batch_size=args.bsize,
+        dataset = FisherTextDatasetMaxLen(
+            args.in_file,
+            tokenizer,
+            max_len=model.config.n_positions,
+            batch_size=args.bsize,
         )
     else:
         raise ValueError(f"Wrong set 'context_type' as {args.context_type}")
 
+    nlls = []
+    ids = []
+    stime = time()
 
-    proc = Processor(model, dataset, device, path_out_dir / f"dataset.pt")
+    if args.context_type == "max_len":
+        obj = load_checkpoint(path_out_dir)
+        if obj:
+            nlls = obj["nlls"]
+            ids = obj["ids"]
+            dataset.ids_done = obj["ids_done"]
+            dataset.sentence_done = obj["sentence_done"]
 
-    # nlls = []
-    # stime = time()
     with torch.no_grad():
-        if args.context_type == "indep":
-            ...
-        #     for batch_text_ids in dataset:
-        #         batch_text_ids = torch.LongTensor(batch_text_ids).to(device=device)
-        #         target_ids = batch_text_ids.clone()
+        pbar = tqdm(dataset, initial=dataset.sentence_done)
 
-        #         outputs = model(batch_text_ids)
+        for i, sample in enumerate(pbar):
+            if args.context_type == "indep":
+                batch_text_ids = sample
+            elif args.context_type == "max_len":
+                batch_text_ids, batch_ids = sample
+                ids.extend(batch_ids)
+            else:
+                raise NotImplementedError
 
-        #         xen_loss = torch.nn.CrossEntropyLoss(reduction='none')
+            batch_text_ids = torch.LongTensor(batch_text_ids).to(device=device)
+            target_ids = batch_text_ids.clone()
 
-        #         # ignore the logits for the last token
-        #         shifted_logits = outputs.logits[..., :-1, :].contiguous()
+            outputs = model(batch_text_ids)
 
-        #         # ignore the targets for the first token
-        #         shifted_targets = target_ids[..., 1:].contiguous()
+            xen_loss = torch.nn.CrossEntropyLoss(reduction="none")
 
-        #         # compute cross entropy loss and return it for every token
-        #         neg_llh = xen_loss(torch.transpose(shifted_logits, 1, 2), shifted_targets)
+            # ignore the logits for the last token
+            shifted_logits = outputs.logits[..., :-1, :].contiguous()
 
-        #         # append to the list of nlls
-        #         nlls.extend(neg_llh.cpu().numpy().tolist())
-        elif args.context_type == "max_len":
-            proc.fwd()
-        else:
-            raise NotImplementedError
+            # ignore the targets for the first token
+            shifted_targets = target_ids[..., 1:].contiguous()
 
-    assert len(proc.nlls) == len(proc.ids), f"nlls {len(proc.nlls)} != utt_ids {len(proc.ids)}"
+            # compute cross entropy loss and return it for every token
+            neg_llh = xen_loss(torch.transpose(shifted_logits, 1, 2), shifted_targets)
 
-    rec_id2nlls, rec_id2ppl = compute_ppl_per_recording(proc.nlls, proc.ids)
+            # append to the list of nlls
+            nlls.extend(neg_llh.cpu().numpy().tolist())
+
+            if args.context_type == "max_len":
+                if i % args.patience_save_checkpoints == 0:
+                    obj = {
+                        "nlls": nlls,
+                        "ids": ids,
+                        "ids_done": dataset.ids_done,
+                        "sentence_done": dataset.sentence_done,
+                    }
+                    save_checkpoint(obj, path_out_dir, keep=args.keep_n_checkpoints)
+
+            pbar.update(batch_text_ids.shape[0])
+
+    if args.context_type == "indep":
+        ids = dataset.utt_ids
+
+    assert len(nlls) == len(ids), f"nlls {len(nlls)} != utt_ids {len(ids)}"
+
+    rec_id2nlls, rec_id2ppl = compute_ppl_per_recording(nlls, ids)
 
     with open(path_out_dir / "rec_id2nlls.pkl", "wb") as fpw:
         pickle.dump(rec_id2nlls, fpw)
@@ -115,9 +145,10 @@ def main():
     with open(path_out_dir / "rec_id2ppl.json", "w", encoding="utf-8") as fpw:
         json.dump(rec_id2ppl, fpw, indent=2, ensure_ascii=False)
 
-    # logger.info(f"Saved in {args.out_dir} Time taken {time() - stime:.2f} sec")
+    logger.info(f"Saved in {args.out_dir} Time taken {time() - stime:.2f} sec")
 
     return 0
+
 
 def parse_arguments():
     """parse command line arguments"""
@@ -126,7 +157,9 @@ def parse_arguments():
         description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument(
-        "--in_file", "-in_file", "-i",
+        "--in_file",
+        "-in_file",
+        "-i",
         required=True,
         help="path to input text file on which PPL shall be computed",
     )
@@ -140,30 +173,60 @@ def parse_arguments():
     #     in a recording""",
     # )
     parser.add_argument(
-        "--out_dir", "--out_dir", "-o",
+        "--out_dir",
+        "--out_dir",
+        "-o",
         required=True,
         help="path to out dir where results are stored",
     )
     parser.add_argument(
-        "--bsize", "--batch_size", "-bsize", "-batch_size", "--sb", "-sb",
+        "--bsize",
+        "--batch_size",
+        "-bsize",
+        "-batch_size",
+        "--sb",
+        "-sb",
         type=int,
         default=128,
-        help="max batch size"
+        help="max batch size",
     )
     parser.add_argument(
-        "--model", "-model", "-m",
+        "--model",
+        "-model",
+        "-m",
         type=str,
         default="gpt2",
         choices=["gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"],
         help="huggingface model",
     )
     parser.add_argument(
-        "--context_type", "-context_type", "--ct", "-ct",
+        "--context_type",
+        "-context_type",
+        "--ct",
+        "-ct",
         choices=["indep", "max_len"],
         default="indep",
         type=str,
         help="""How much context to use? indep is independent utterances, max_len is
         max length constrained by the pretrained model which could be 512 or 1024 tokens""",
+    )
+    parser.add_argument(
+        "--keep_n_checkpoints",
+        "--knc",
+        "-keep_n_checkpoints",
+        "-knc",
+        type=int,
+        default=5,
+        help="Number how many checkpoints keep saved",
+    )
+    parser.add_argument(
+        "--patience_save_checkpoints",
+        "--psc",
+        "-patience_save_checkpoints",
+        "-psc",
+        type=int,
+        default=100,
+        help="Number how many batch processed for checkpointing",
     )
     parser.add_argument(
         "--no_cuda",
@@ -181,12 +244,14 @@ def parse_arguments():
         help="Download only model from HuggingFace",
     )
     parser.add_argument(
-        "--but_gpu", "-but_gpu",
+        "--but_gpu",
+        "-but_gpu",
         action="store_true",
         help="Use BUT's procedure for access to GPU",
     )
     parser.add_argument(
-        "--verbose", "-v",
+        "--verbose",
+        "-v",
         action="store_true",
         help="print logging info on stdout",
     )
--- lms/src/utils.py
+++ lms/src/utils.py
@@ -2,200 +2,88 @@ import logging
 import sys
 import os
 import torch
+import glob
 import numpy as np
-from tqdm import tqdm
-from typing import Iterator, List, Tuple
+from typing import Any, List, Tuple, Dict
 from datetime import datetime
 from collections import defaultdict
 from torch.utils.data import IterableDataset
 
 
-class Processor:
-    def __init__(self, model, dataloader, device, checkpoint, ):
-        self.model = model
-        self.dataloader = dataloader
-        self.device = device
-        self.checkpoint = checkpoint
+def save_checkpoint(obj, exp_dir: str, keep: int = 5) -> None:
+    r"""Save checkpoint (variables)
 
-        self.nlls = []
-        self.ids = []
+    Args:
+        exp_dir (str): Path to experiment's directory
+        keep (int): Number of how many checkpoint we want keep (Default: 5)
+    """
+    logger = logging.getLogger()
+    now_str = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    fname = exp_dir / f"checkpoint-{now_str}.pt"
 
-        self.load_checkpoint()
+    torch.save(obj, fname)
 
-    def fwd(self):
-        # stime = time()
-        pbar = tqdm(self.dataloader, initial=self.dataloader.sentence_done)
-        # pbar.update(self.dataloader.sentence_done)
-        for i, (batch_text_ids, batch_ids, i_record) in enumerate(pbar):
-            batch_text_ids = torch.LongTensor(batch_text_ids).to(device=self.device)
-            target_ids = batch_text_ids.clone()
+    checkpoints = glob.glob(str(exp_dir / f"checkpoint-*"))
+    if len(checkpoints) > keep:
+        for c in checkpoints[: len(checkpoints) - keep]:
+            os.remove(c)
 
-            outputs = self.model(batch_text_ids)
+    return
 
-            xen_loss = torch.nn.CrossEntropyLoss(reduction='none')
 
-            # ignore the logits for the last token
-            shifted_logits = outputs.logits[..., :-1, :].contiguous()
+def load_checkpoint(exp_dir: str) -> Dict[str, Any]:
+    logger = logging.getLogger()
+    checkpoints = glob.glob(str(exp_dir / f"checkpoint-*"))
+    if checkpoints:
+        for i in range(1, len(checkpoints) + 1):
+            try:
+                obj = torch.load(checkpoints[-i])
+                logger.info(f"Checkpoint {checkpoints[-i]} loaded")
+                break
+            except RuntimeError:
+                logger.info(
+                    f"Checkpoint {checkpoints[-i]} is corrupted ... try another one"
+                )
+    else:
+        obj = {}
 
-            # ignore the targets for the first token
-            shifted_targets = target_ids[..., 1:].contiguous()
-
-            # compute cross entropy loss and return it for every token
-            neg_llh = xen_loss(torch.transpose(shifted_logits, 1, 2), shifted_targets)
-
-            self.nlls.extend(neg_llh.cpu().numpy().tolist())
-            self.ids.extend(batch_ids)
-
-            pbar.update(batch_text_ids.shape[0])
-            pbar.set_postfix({'Recordings': f"{i_record}/{self.dataloader.nrecording}"})
-
-            if i % 100 == 0:
-                self.save_checkpoint()
-
-    def save_checkpoint(self):
-        # print("(save) nlls_score", len(self.nlls))
-        # print("(save) ids_score", len(self.ids))
-        # print("(save) sentence_processed", self.dataloader.sentence_done)
-        torch.save({
-            "id_processed": self.dataloader.ids_done,
-            "sentence_processed": self.dataloader.sentence_done,
-            "nlls_score": self.nlls,
-            "ids_score": self.ids,
-        }, self.checkpoint)
-
-    def load_checkpoint(self):
-        if os.path.isfile(self.checkpoint):
-            checkpoint = torch.load(self.checkpoint)
-            self.dataloader.ids_done = checkpoint["id_processed"]
-            self.dataloader.sentence_done = checkpoint["sentence_processed"]
-            self.nlls = checkpoint["nlls_score"]
-            self.ids = checkpoint["ids_score"]
-            # print("(load) nlls_score", len(self.nlls))
-            # print("(load) ids_score", len(self.ids))
-            # print("(load) sentence_processed", self.dataloader.sentence_done)
-
-class FisherTextDataset(IterableDataset):
-    """ Dataset """
-
-    def __init__(self,
-                 fname: str, tokenizer, batch_size: int = 128,
-                 context_type: str = "indep", max_len: int = 128):
-        """ Init """
-        self.tokenizer = tokenizer
-        self.max_len = max_len
-        self.batch_size = batch_size
-        self.context_type = context_type
-        self.utt_id2text, self.rec_id2tokens = self._load_text(fname)
-        self.nrecording = len(self.rec_id2tokens)
-
-        self.ids_done = []
-        self.sentence_done = 0
-
-    def __iter__(self):
-        return iter(self._next_batch())
-
-    def _load_text(self, fname):
-
-        def recid_time(x):
-            rec, _, start, end = x[0].split("-")
-            return "-".join((rec, start, end))
-
-        utt_id2text = {}
-        rec_id2tokens = defaultdict(list)
-
-        with open(fname, "r", encoding="utf-8") as fd:
-            for line in fd:
-                utt_id, text = line.strip().split(None, maxsplit=1)
-                if utt_id in utt_id2text:
-                    print(f"Duplicate utt id: {utt_id} ignoring", file=sys.stderr)
-                else:
-                    utt_id2text[utt_id] = text
-
-        utt_ids_sorted = sorted(utt_id2text.items(), key=lambda x: recid_time(x))
-
-        for utt_id, text in utt_ids_sorted:
-            rec_id = utt_id.split("-", maxsplit=1)[0]
-            rec_id2tokens[rec_id].extend(self.tokenizer(text)['input_ids'])
-            rec_id2tokens[rec_id].append(self.tokenizer.eos_token_id)
-
-        return utt_id2text, rec_id2tokens
-
-    def __len__(self):
-        nsentence = 0
-        for _, v in self.rec_id2tokens.items():
-            if len(v) < self.max_len:
-                nsentence += 1
-            else:
-                nsentence += (1 + (len(v) - self.max_len))
-
-        return nsentence
-
-    def _next_batch(self):
-        for i, (k, v) in enumerate(self.rec_id2tokens.items(), start=1):
-
-            if k in self.ids_done:
-                continue
-
-            if len(v) < self.max_len:
-                yield [v], [k], i
-                self.sentence_done += 1
-                continue
-
-            buffer = v[:self.max_len]
-            batch = []
-            rec_ids = []
-            for ii in range(self.max_len, len(v)):
-                output_seq = buffer[:]
-
-                batch.append(output_seq)
-                rec_ids.append(k)
-
-                if len(batch) == self.batch_size:
-                    yield batch, rec_ids, i
-                    self.sentence_done += len(batch)
-                    batch.clear()
-                    rec_ids.clear()
-                buffer.pop(0)
-                buffer.append(v[ii])
-
-            # Non-complete batch
-            if batch:
-                yield batch, rec_ids, i
-                self.sentence_done += len(batch)
-
-            self.ids_done.append(k)
+    return obj
 
 
 class FisherTextDatasetIndep(IterableDataset):
-    """ Dataset """
+    """Dataset"""
 
     def __init__(self, fname: str, tokenizer, batch_size: int = 128):
-        """ Init """
+        """Init"""
 
         self.batch_size = batch_size
         self.tokenizer = tokenizer
         self.text_ids, self.lengths, self.utt_ids = self._load_key_text_file(fname)
         self.bins, self.counts = np.unique(self.lengths, return_counts=True)
+        self.sentence_done = 0
 
     def __iter__(self):
         return iter(self._next_batch())
 
+    def __len__(self):
+        return len(self.utt_ids)
+
     def _next_batch(self):
-        """ Generate batches """
+        """Generate batches"""
 
         bno = 1  # batch number
         offset = 0
         for bin, count in zip(self.bins, self.counts):
-            for i in range(offset, offset+count, self.batch_size):
+            for i in range(offset, offset + count, self.batch_size):
                 start = i
-                end = i+self.batch_size
-                if i+self.batch_size > offset+count:
-                    end = offset+count
-                print(f"\r batch {bno:5d} length bin {bin:3d} start ix {start:8d} : {end:8d} / {len(self.text_ids):8d}", end=" ", file=sys.stderr)
+                end = i + self.batch_size
+                if i + self.batch_size > offset + count:
+                    end = offset + count
+                # print(f"\r batch {bno:5d} length bin {bin:3d} start ix {start:8d} : {end:8d} / {len(self.text_ids):8d}", end=" ", file=sys.stderr)
                 yield self.text_ids[start:end]
                 bno += 1
             offset += count
-            print()
+            # print()
 
     def _load_key_text_file(self, fname, ignore_one_tok: bool = True):
         """Load key text file, tokenize and add end_of_token"""
@@ -211,10 +99,16 @@ class FisherTextDatasetIndep(IterableDataset):
                 line = line.strip()
                 utt_id, text = line.split(None, maxsplit=1)
                 if utt_id in utt_id2text:
-                    print("Duplicate utt id:", utt_id, utt_id2text[utt_id], "ignoring", file=sys.stderr)
+                    print(
+                        "Duplicate utt id:",
+                        utt_id,
+                        utt_id2text[utt_id],
+                        "ignoring",
+                        file=sys.stderr,
+                    )
 
                 else:
-                    tok_ids = self.tokenizer(text)['input_ids']
+                    tok_ids = self.tokenizer(text)["input_ids"]
                     tok_ids.append(self.tokenizer.eos_token_id)
                     tok_ids.insert(0, self.tokenizer.bos_token_id)
 
@@ -240,14 +134,100 @@ class FisherTextDatasetIndep(IterableDataset):
 
         return sorted_text_ids, lengths[sort_ixs], sorted_utt_ids
 
-    def print_recid2tokens(self):
-        recid2tokens = defaultdict(list)
-        recid2tokens = defaultdict(int)
-        for text_ids, utt_id in zip(self.text_ids, self.utt_ids):
-            rec_id = utt_id.split("-", maxsplit=1)[0]
-            recid2tokens[rec_id] += text_ids
 
-        return recid2tokens
+class FisherTextDatasetMaxLen(IterableDataset):
+    """Dataset"""
+
+    def __init__(
+        self,
+        fname: str,
+        tokenizer,
+        batch_size: int = 128,
+        context_type: str = "indep",
+        max_len: int = 128,
+    ):
+        """Init"""
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.batch_size = batch_size
+        self.context_type = context_type
+        self.utt_id2text, self.rec_id2tokens = self._load_text(fname)
+        self.nrecording = len(self.rec_id2tokens)
+
+        self.ids_done = []
+        self.sentence_done = 0
+
+    def __iter__(self):
+        return iter(self._next_batch())
+
+    def _load_text(self, fname):
+        def recid_time(x):
+            rec, _, start, end = x[0].split("-")
+            return "-".join((rec, start, end))
+
+        utt_id2text = {}
+        rec_id2tokens = defaultdict(list)
+
+        with open(fname, "r", encoding="utf-8") as fd:
+            for line in fd:
+                utt_id, text = line.strip().split(None, maxsplit=1)
+                if utt_id in utt_id2text:
+                    print(f"Duplicate utt id: {utt_id} ignoring", file=sys.stderr)
+                else:
+                    utt_id2text[utt_id] = text
+
+        utt_ids_sorted = sorted(utt_id2text.items(), key=lambda x: recid_time(x))
+
+        for utt_id, text in utt_ids_sorted:
+            rec_id = utt_id.split("-", maxsplit=1)[0]
+            rec_id2tokens[rec_id].extend(self.tokenizer(text)["input_ids"])
+            rec_id2tokens[rec_id].append(self.tokenizer.eos_token_id)
+
+        return utt_id2text, rec_id2tokens
+
+    def __len__(self):
+        nsentence = 0
+        for _, v in self.rec_id2tokens.items():
+            if len(v) < self.max_len:
+                nsentence += 1
+            else:
+                nsentence += 1 + (len(v) - self.max_len)
+
+        return nsentence
+
+    def _next_batch(self):
+        for k, v in self.rec_id2tokens.items():
+            if k in self.ids_done:
+                continue
+
+            if len(v) < self.max_len:
+                yield [v], [k]
+                self.sentence_done += 1
+                continue
+
+            buffer = v[: self.max_len]
+            batch = []
+            rec_ids = []
+            for ii in range(self.max_len, len(v)):
+                output_seq = buffer[:]
+
+                batch.append(output_seq)
+                rec_ids.append(k)
+
+                if len(batch) == self.batch_size:
+                    yield batch, rec_ids
+                    self.sentence_done += len(batch)
+                    batch.clear()
+                    rec_ids.clear()
+                buffer.pop(0)
+                buffer.append(v[ii])
+
+            # Non-complete batch
+            if batch:
+                yield batch, rec_ids
+                self.sentence_done += len(batch)
+
+            self.ids_done.append(k)
 
 
 def create_logger(log_file_base, verbose: bool):
@@ -287,7 +267,6 @@ def compute_ppl_per_recording(nlls: list, utt_ids: list) -> Tuple[dict, dict]:
 
     rec_id2nlls = {}
     for i, utt_id in enumerate(utt_ids):
-
         rec_id = utt_id.split("-", maxsplit=1)[0]
 
         if rec_id not in rec_id2nlls:
