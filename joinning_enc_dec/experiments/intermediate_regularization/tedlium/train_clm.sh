bb7d7f4e9ca0fed28bc3517c483e58e9e3e7084c
lakoc <xpolok03@stud.fit.vutbr.cz>
2023-10-24 20:23:55 +0200

    clm training

--- joinning_enc_dec/experiments/intermediate_regularization/tedlium/train_clm.sh
+++ joinning_enc_dec/experiments/intermediate_regularization/tedlium/train_clm.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
-#SBATCH --job-name TED
+#SBATCH --job-name TED_LM
 #SBATCH --account OPEN-28-58
 #SBATCH --partition qgpu
-#SBATCH --gpus 4
+#SBATCH --gpus 1
 #SBATCH --nodes 1
-#SBATCH --time 2-00:00:00
-#SBATCH --output=/mnt/proj1/open-28-58/lakoc/LoCo-ASR/outputs/tedlium_clm.out
+#SBATCH --time 1-00:00:00
+#SBATCH --output=/mnt/proj1/open-28-58/lakoc/LoCo-ASR/outputs/tedlium_clm1.out
 
-EXPERIMENT="tedlium_clm"
+EXPERIMENT="tedlium_clm1"
 PROJECT="TED_CLM"
 WORK_DIR="/mnt/proj1/open-28-58/lakoc/LoCo-ASR"
 EXPERIMENT_PATH="${WORK_DIR}/experiments/${PROJECT}_${EXPERIMENT}"
@@ -23,17 +23,20 @@ source activate loco_asr
 
 cd $WORK_DIR
 
-torchrun --standalone \
-  --nnodes=1 \
-  --nproc-per-node=4 \
+python \
   joinning_enc_dec/src/trainers/train_clm.py \
   --model_type gpt2 \
-  --config_overrides="n_embd=1024,n_head=8,n_layer=16" \
+  --config_overrides="n_embd=1024,n_head=8,n_layer=16,vocab_size=500,bos_token_id=0,eos_token_id=1,n_positions=256" \
   --tokenizer_name="Lakoc/ted_bpe500" \
   --dataset_name LIUM/tedlium \
   --dataset_config_name release3 \
-  --per_device_train_batch_size 8 \
-  --per_device_eval_batch_size 8 \
+  --per_device_train_batch_size 128 \
+  --per_device_eval_batch_size 128 \
+  --auto_find_batch_size \
   --do_train \
   --do_eval \
+  --num_train_epochs=40 \
+  --warmup_steps=25000 \
+  --learning_rate="1e-3" \
+  --bf16 \
   --output_dir $EXPERIMENT_PATH
