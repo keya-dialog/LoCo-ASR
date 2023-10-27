58279f0520c7f9eac7c47750d8f28fb0682c0f14
lakoc <xpolok03@stud.fit.vutbr.cz>
2023-10-28 00:01:36 +0200

    lm scripts

--- joinning_enc_dec/experiments/intermediate_regularization/tedlium/train_clm_llama.sh
+++ joinning_enc_dec/experiments/intermediate_regularization/tedlium/train_clm_llama.sh
@@ -26,7 +26,7 @@ cd $WORK_DIR
 python \
   joinning_enc_dec/src/trainers/train_clm.py \
   --model_type llama \
-  --config_overrides="hidden_size=256,intermediate_size=1024,num_attention_heads=8,num_hidden_layers=8,vocab_size=500,bos_token_id=0,eos_token_id=1,pad_token_id=3,max_position_embeddings=512" \
+  --config_overrides="hidden_size=512,intermediate_size=2048,num_attention_heads=8,num_hidden_layers=12,vocab_size=500,bos_token_id=0,eos_token_id=1,pad_token_id=3,max_position_embeddings=512" \
   --tokenizer_name="Lakoc/ted_bpe500" \
   --dataset_name LIUM/tedlium \
   --dataset_config_name release3 \
