f61b9768fd5824daca616a4229257278fc6927d4
lakoc <xpolok03@stud.fit.vutbr.cz>
2023-10-28 22:35:56 +0200

    smaller LM

---
+++ joinning_enc_dec/experiments/intermediate_regularization/tedlium/train_clm_higher_warmpup_smaller_model.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+#SBATCH --job-name TED_LM
+#SBATCH --account OPEN-28-58
+#SBATCH --partition qgpu
+#SBATCH --gpus 1
+#SBATCH --nodes 1
+#SBATCH --time 1-00:00:00
+#SBATCH --output=/mnt/proj1/open-28-58/lakoc/LoCo-ASR/outputs/tedlium_clm_gpt2_smaller.out
+
+EXPERIMENT="tedlium_clm_gpt2_smaller"
+PROJECT="TED_CLM"
+WORK_DIR="/mnt/proj1/open-28-58/lakoc/LoCo-ASR"
+EXPERIMENT_PATH="${WORK_DIR}/experiments/${PROJECT}_${EXPERIMENT}"
+
+export WANDB_PROJECT=$PROJECT
+export WANDB_RUN_ID="${EXPERIMENT}"
+export HF_HOME="${WORK_DIR}/huggingface_cache"
+export PYTHONPATH="${PYTHONPATH}:${WORK_DIR}/joinning_enc_dec/src"
+export OMP_NUM_THREADS=64
+
+conda deactivate
+source activate loco_asr
+
+cd $WORK_DIR
+
+python \
+  joinning_enc_dec/src/trainers/train_clm.py \
+  --model_type gpt2 \
+  --config_overrides="n_embd=256,n_head=8,n_layer=12,vocab_size=500,bos_token_id=0,eos_token_id=1,n_positions=512" \
+  --tokenizer_name="Lakoc/ted_bpe500" \
+  --dataset_name LIUM/tedlium \
+  --pad_token_id=3 \
+  --dataset_config_name release3 \
+  --per_device_train_batch_size 64 \
+  --per_device_eval_batch_size 64 \
+  --auto_find_batch_size \
+  --do_train \
+  --do_eval \
+  --logging_steps="5" \
+  --save_strategy="steps" \
+  --save_steps="500" \
+  --evaluation_strategy="steps" \
+  --eval_steps="500" \
+  --num_train_epochs=50 \
+  --warmup_steps=5000 \
+  --learning_rate="5e-4" \
+  --bf16 \
+  --save_total_limit="2" \
+  --output_dir $EXPERIMENT_PATH \
+  --load_best_model_at_end
