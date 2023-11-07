a9a7002dc7883eeb8317c3b0378fcb0d7e318e51
lakoc <xpolok03@stud.fit.vutbr.cz>
2023-11-07 01:26:44 +0100

    wpe fixes

--- joinning_enc_dec/experiments/intermediate_regularization/tedlium/tedlium_ebranchformer_tiny_esp_no_aug_uni_fixed_pos.sh
+++ joinning_enc_dec/experiments/intermediate_regularization/tedlium/tedlium_ebranchformer_tiny_esp_no_aug_uni_fixed_pos.sh
@@ -5,9 +5,9 @@
 #SBATCH --gpus 4
 #SBATCH --nodes 1
 #SBATCH --time 2-00:00:00
-#SBATCH --output=/mnt/proj1/open-28-58/lakoc/LoCo-ASR/outputs/tedlium_ebranchformer_tiny_esp_no_aug_uni_fixed_pos_proper_scoring.out
+#SBATCH --output=/mnt/proj1/open-28-58/lakoc/LoCo-ASR/outputs/tedlium_ebranchformer_tiny_esp_no_aug_uni500_fixed_pos_proper_scoring.out
 
-EXPERIMENT="tedlium_ebranchformer_tiny_esp_no_aug_uni_fixed_pos_proper_scoring"
+EXPERIMENT="tedlium_ebranchformer_tiny_esp_no_aug_uni500_fixed_pos_proper_scoring"
 PROJECT="TED"
 WORK_DIR="/mnt/proj1/open-28-58/lakoc/LoCo-ASR"
 EXPERIMENT_PATH="${WORK_DIR}/experiments/${PROJECT}_${EXPERIMENT}"
