c0b6a0a29a6624b6cd8acd0a7944620fd76688c0
lakoc <xpolok03@stud.fit.vutbr.cz>
2023-11-12 23:01:54 +0100

    cv czech

--- joinning_enc_dec/experiments/intermediate_regularization/common_voice/cs/common_voice_ebranchformer_czech.sh
+++ joinning_enc_dec/experiments/intermediate_regularization/common_voice/cs/common_voice_ebranchformer_czech.sh
@@ -5,9 +5,9 @@
 #SBATCH --gpus 1
 #SBATCH --nodes 1
 #SBATCH --time 06:00:00
-#SBATCH --output=/mnt/proj1/open-28-58/lakoc/LoCo-ASR/outputs/common_voice_AED_ebranchformer8.out
+#SBATCH --output=/mnt/proj1/open-28-58/lakoc/LoCo-ASR/outputs/common_voice_AED_ebranchformer_cs.out
 
-EXPERIMENT="common_voice_AED_ebranchformer8"
+EXPERIMENT="common_voice_AED_ebranchformer_cs"
 PROJECT="CommonVoice"
 WORK_DIR="/mnt/proj1/open-28-58/lakoc/LoCo-ASR"
 EXPERIMENT_PATH="${WORK_DIR}/experiments/${PROJECT}_${EXPERIMENT}"
@@ -38,7 +38,9 @@ python joinning_enc_dec/src/trainers/train_tokenizer.py \
   --skip_if_exists="${USER}/${TOKENIZER_NAME}"
 
 
-python \
+torchrun --standalone \
+  --nnodes=1 \
+  --nproc-per-node=4 \
   joinning_enc_dec/src/trainers/AED_from_enc_dec.py \
   --dataset_name="mozilla-foundation/common_voice_13_0" \
   --dataset_config="cs" \
@@ -46,18 +48,18 @@ python \
   --min_duration_in_seconds="0.0" \
   --base_encoder_model="Lakoc/fisher_ebranchformer_enc_12_layers_fixed" \
   --feature_extractor_name="Lakoc/fisher_log_mel_extractor" \
-  --base_decoder_model="Lakoc/gpt2_256h_6l_add_head3" \
+  --base_decoder_model="Lakoc/gpt2_tiny_decoder_6_layers" \
   --tokenizer_name="Lakoc/CV_cs_uni500" \
   --output_dir=$EXPERIMENT_PATH \
-  --gradient_accumulation_steps="2" \
-  --learning_rate="1e-3" \
-  --warmup_steps="2000" \
+  --gradient_accumulation_steps="1" \
+  --learning_rate="2e-4" \
+  --warmup_steps="5000" \
   --logging_steps="5" \
   --save_strategy="steps" \
-  --save_steps="300" \
+  --save_steps="500" \
   --evaluation_strategy="steps" \
-  --eval_steps="300" \
-  --per_device_train_batch_size="64" \
+  --eval_steps="500" \
+  --per_device_train_batch_size="96" \
   --per_device_eval_batch_size="48" \
   --report_to="wandb" \
   --optim="adamw_torch" \
@@ -67,13 +69,13 @@ python \
   --metric_for_best_model="eval_wer" \
   --remove_unused_columns="False" \
   --save_total_limit="5" \
-  --num_train_epochs="100" \
+  --num_train_epochs="200" \
   --num_beams="4" \
   --max_len="512" \
   --greater_is_better="False" \
   --group_by_length="True" \
   --bf16 \
-  --ctc_weight="0.6" \
+  --ctc_weight="0.3" \
   --bos_token="<s>" \
   --eos_token="</s>" \
   --pad_token="<pad>" \
@@ -93,10 +95,9 @@ python \
   --do_train \
   --evaluation_splits validation test \
   --do_eval \
-  --decoding_ctc_weight="0.6" \
+  --decoding_ctc_weight="0.3" \
   --eval_beam_factor="5" \
   --validation_slice 500 \
-  --track_ctc_loss \
-  --joint_decoding_during_training
+  --track_ctc_loss
 
 cp /mnt/proj1/open-28-58/lakoc/LoCo-ASR/outputs/LoCo-$EXPERIMENT.out $EXPERIMENT_PATH/
