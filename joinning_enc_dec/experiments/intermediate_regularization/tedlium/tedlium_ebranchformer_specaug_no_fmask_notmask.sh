34c625541dbc07ac021091c8eb070fbd274e688c
lakoc <xpolok03@stud.fit.vutbr.cz>
2023-11-13 18:21:34 +0100

    specaugment

---
+++ joinning_enc_dec/experiments/intermediate_regularization/tedlium/tedlium_ebranchformer_specaug_no_fmask_notmask.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/bash
+#SBATCH --job-name TED
+#SBATCH --account OPEN-28-57
+#SBATCH --partition qgpu_exp
+#SBATCH --gpus 4
+#SBATCH --nodes 1
+#SBATCH --time 1:00:00
+#SBATCH --output=/mnt/proj1/open-28-58/lakoc/LoCo-ASR/outputs/tedlium_ebranchformer_specaug_no_warp_tmask.out
+
+EXPERIMENT="tedlium_ebranchformer_specaug_no_warp_tmask"
+PROJECT="TED"
+WORK_DIR="/mnt/proj1/open-28-58/lakoc/LoCo-ASR"
+EXPERIMENT_PATH="${WORK_DIR}/experiments/${PROJECT}_${EXPERIMENT}"
+
+export WANDB_PROJECT=$PROJECT
+export WANDB_RUN_ID="${EXPERIMENT}"
+export HF_HOME="${WORK_DIR}/huggingface_cache"
+export PYTHONPATH="${PYTHONPATH}:${WORK_DIR}/joinning_enc_dec/src"
+export OMP_NUM_THREADS=64
+
+conda deactivate
+source activate loco_asr
+
+cd $WORK_DIR
+
+torchrun --standalone \
+  --nnodes=1 \
+  --nproc-per-node=4 \
+  joinning_enc_dec/src/trainers/AED_from_enc_dec.py \
+  --dataset_name="LIUM/tedlium" \
+  --dataset_config="release3" \
+  --max_duration_in_seconds="40.0" \
+  --min_duration_in_seconds="0.0" \
+  --base_encoder_model="Lakoc/fisher_ebranchformer_enc_12_layers_fixed" \
+  --feature_extractor_name="Lakoc/fisher_log_mel_extractor" \
+  --base_decoder_model="Lakoc/gpt2_256h_6l_add_head3" \
+  --tokenizer_name="Lakoc/ted_uni500" \
+  --output_dir=$EXPERIMENT_PATH \
+  --gradient_accumulation_steps="1" \
+  --learning_rate="2e-3" \
+  --warmup_steps="15000" \
+  --logging_steps="10" \
+  --save_strategy="epoch" \
+  --evaluation_strategy="epoch" \
+  --per_device_train_batch_size="48" \
+  --per_device_eval_batch_size="48" \
+  --report_to="wandb" \
+  --optim="adamw_torch" \
+  --dataloader_num_workers="24" \
+  --length_column_name="input_len" \
+  --load_best_model_at_end="True" \
+  --metric_for_best_model="eval_wer" \
+  --remove_unused_columns="False" \
+  --save_total_limit="5" \
+  --num_train_epochs="100" \
+  --num_beams="4" \
+  --max_len="512" \
+  --greater_is_better="False" \
+  --group_by_length="True" \
+  --bf16 \
+  --ctc_weight="0.3" \
+  --bos_token="<s>" \
+  --eos_token="</s>" \
+  --pad_token="<pad>" \
+  --mask_token="<mask>" \
+  --lsm_factor="0.1" \
+  --use_fbanks \
+  --apply_augmentations \
+  --predict_with_generate \
+  --early_stopping_patience="100" \
+  --preprocessing_num_workers="128" \
+  --fix_apostrophes \
+  --remove_train_unks \
+  --wandb_predictions_to_save=600 \
+  --from_encoder_decoder_config \
+  --weight_decay="1e-6" \
+  --max_grad_norm="5.0" \
+  --decoder_pos_emb_fixed \
+  --do_train \
+  --do_eval \
+  --decoding_ctc_weight="0.3" \
+  --eval_beam_factor="10" \
+  --external_lm_weight="0.5" \
+  --apply_spec_augment \
+  --external_lm="Lakoc/TED_CLM_gpt2_tedlium3" \
+  --config_overrides="encoder_apply_time_warp=True,encoder_mask_feature_prob=0.00,encoder_mask_time_prob=0.00" \
+  --evaluation_splits validation test
+
+cp /mnt/proj1/open-28-58/lakoc/LoCo-ASR/outputs/LoCo-$EXPERIMENT.out $EXPERIMENT_PATH/
