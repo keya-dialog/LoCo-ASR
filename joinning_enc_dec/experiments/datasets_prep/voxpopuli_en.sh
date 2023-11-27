6bbbae2e79bc0a131acde1276867fa8754ef2471
lakoc <xpolok03@stud.fit.vutbr.cz>
2023-11-27 23:02:07 +0100

    dataset prep jobs

---
+++ joinning_enc_dec/experiments/datasets_prep/voxpopuli_en.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+#SBATCH --job-name CommonVoice
+#SBATCH --account OPEN-28-57
+#SBATCH --partition qcpu
+#SBATCH --time 24:00:00
+
+EXPERIMENT="vox_populi"
+PROJECT="CommonVoice"
+WORK_DIR="/mnt/proj1/open-28-58/lakoc/LoCo-ASR"
+EXPERIMENT_PATH="${WORK_DIR}/experiments/${PROJECT}_${EXPERIMENT}"
+USER="lakoc"
+TOKENIZER_NAME="ted_uni500"
+
+export WANDB_PROJECT=$PROJECT
+export WANDB_RUN_ID=$EXPERIMENT
+export HF_HOME="${WORK_DIR}/huggingface_cache"
+export PYTHONPATH="${PYTHONPATH}:${WORK_DIR}/joinning_enc_dec/src"
+export OMP_NUM_THREADS=64
+
+conda deactivate
+source activate loco_asr
+
+cd $WORK_DIR
+  
+python \
+  joinning_enc_dec/src/trainers/AED_from_enc_dec.py \
+  --dataset_name="facebook/voxpopuli" \
+  --dataset_config="en" \
+  --feature_extractor_name="Lakoc/log_80mel_extractor_16k" \
+  --tokenizer_name="${USER}/${TOKENIZER_NAME}" \
+  --output_dir=$EXPERIMENT_PATH \
+  --report_to="none" \
+  --length_column_name="input_len" \
+  --text_column_name="normalized_text" \
+  --preprocessing_num_workers="128" \
+  --fix_apostrophes \
+  --train_split train \
+  --preprocess_dataset_only
